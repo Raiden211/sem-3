437872564717b9b3353066d55e92a40beaea0e22
Biji Naga <72716980+Raiden211@users.noreply.github.com>
2023-08-08 16:14:20 +0800

    Add files via upload

---
+++ php/dasarnya/database/databasis/index2.php
@@ -0,0 +1,136 @@
+<?php 
+    include 'config.php';
+    function hitungna($a,$b)
+    {
+        $res = (($a*4/10)+($b*6/10));
+        return $res;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Tutor nya ada di halaman berikut : </p>
+    <a href="index1.php">Tutor1</a>
+    <a href="index3.php">Tutor3</a>
+<form action="#" method="post">
+        <p>Nama : </p> 
+        <input type="text" name="nama2"> <br>
+        <p>Nilai UTS : </p>
+        <input type="text" name="nilai1"> <br>
+        <p>Nilai UAS : </p>
+        <input type="text" name="nilai2"> <br>
+        <br>
+        <button type="submit" name="add2">Tambah</button>
+        <button type="submit" name="delete">Delete</button>
+        <?php 
+            $name = $_POST['nama2'];
+            $nilai1 = $_POST['nilai1'];
+            $nilai2 = $_POST['nilai2'];
+            $na = hitungna($nilai1,$nilai2);
+            if($name == "")
+            {
+                echo "Nama gaboleh kosong<br>";
+            }
+            if($nilai1 == "")
+            {
+                $nilai1 = 0;
+            }
+            if($nilai2 == "")
+            {
+                $nilai2 = 0;
+            }
+            else 
+            {
+                if($na >= 80)
+                {
+                    $grade = "A";
+                }
+                else if($na >= 75)
+                {
+                    $grade = "B+";
+                }
+                else if($na >= 70)
+                {
+                    $grade = "B";
+                }
+                else if($na >= 64)
+                {
+                    $grade = "C+";
+                }
+                else if($na > 55)
+                {
+                    $grade = "C";
+                }
+                else if($na >= 45)
+                {
+                    $grade = "D";
+                }
+                else if($na < 45 || $nilai2 == 0 || $nilai2 == "")
+                {
+                    $grade = "E";
+                }
+                $sql = "insert into label2 (name,nilai1,nilai2,na,grade) 
+                values ('$name','$nilai1','$nilai2','$na','$grade')";
+                $query = mysqli_query($conn,$sql);
+                if($query)
+                {
+                    echo "Data berhasil masuk";
+                }
+                else 
+                {
+                    echo "Ada yang salah";
+                }
+            }
+        ?>
+    </form>
+    // result 
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Nilai UTS</th>
+            <th>Nilai UAS</th>
+            <th>Nilai Akhir</th>
+            <th>Grade</th>
+        </tr>
+        <?php 
+            $sql = "SELECT * FROM label2";
+            $res = mysqli_query($conn,$sql);
+            while ($row = mysqli_fetch_array($res))
+            {
+                ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['name'] ?></td>
+                    <td><?= $row['nilai1'] ?></td>
+                    <td><?= $row['nilai2'] ?></td>
+                    <td><?= $row['na'] ?></td>
+                    <td><?= $row['grade'] ?></td>
+                </tr>
+        <?php
+            }
+        ?>
+        <?php 
+            if(isset($_GET['delete']))
+            {
+                $sql = "Delete from label2";
+                $res = mysqli_query($conn,$sql);
+                if($res)
+                {
+                    echo "data terhapus";
+                }
+                else 
+                {
+                    echo "error";
+                }
+            }
+        ?>
+    </table> 
+</body>
+</html>
---
+++ php/dasarnya/database/databasis/index3.php
@@ -0,0 +1,108 @@
+<?php 
+    include 'config.php';
+?>
+// search engine tidak sederhana 
+// Berikut adalah cara membuat search engine yang baik dan benar :v
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Tutor nya ada di halaman berikut : </p>
+    <a href="index2.php">Tutor2</a>
+    <a href="index3.php">Tutor3</a>
+    <form action="#" method="post">
+        <p>Nama : </p> 
+        <input type="text" name="nama"> <br>
+        <p>Umur : </p>
+        <input type="text" name="usia"> <br> <br>
+        <button type="submit" name="add">Tambah</button>
+        <?php
+            $nama = $_POST['nama'];
+            $usia = $_POST['usia'];
+            if($nama == "" || $usia == "" || $usia < 1)
+            {
+                echo "Ada yg kosong/usia g sesuai ketentuan.";
+            }
+            else 
+            {
+                $sql = "INSERT INTO label3 (nama,usia)
+                values ('$nama','$usia')";
+                $res = mysqli_query($conn,$sql);
+                if($res)
+                {
+                    echo "Data masuk";
+                }
+                else 
+                {
+                    echo "Salah keknya";
+                }
+            }
+        ?>
+    </form>
+    //Result 
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Usia</th>
+        </tr>
+        <?php 
+            $sql = "SELECT * FROM label3";
+            $res = mysqli_query($conn,$sql);
+            while ($row = mysqli_fetch_array($res))
+            {
+                ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['nama'] ?></td>
+                    <td><?= $row['usia'] ?></td>
+                </tr>
+        <?php
+            }
+        ?>
+    </table>
+    // search data
+    <form action="#" method="post">
+        <p>Cari Nama : </p> 
+        <input type="text" name="search"> <br> <br>
+        <button type="submit" name="add">Cari</button>
+        <?php 
+            $search = $_POST['search'];
+            if($search == "")
+            {
+                echo "Jangan kosong";
+            }
+            else 
+            {
+            ?>
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Usia</th>
+                </tr>
+            <?php 
+            $sql = "SELECT * FROM label3 where nama like '%$search%'";
+            $res = mysqli_query($conn,$sql);
+            while ($row = mysqli_fetch_array($res))
+            {
+                ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['nama'] ?></td>
+                    <td><?= $row['usia'] ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        <?php        
+            }
+        ?>
+        </table>
+    </form>
+</body>
+</html>
